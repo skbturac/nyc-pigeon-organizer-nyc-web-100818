529ec3e3ac2d8b2307673da50ddf88cf059d3e60
Suheyl Turac <skbsturac@live.com>
2018-10-04 16:28:30 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  org_pigeon = {}
+  data.each do |assign_to, info|
+    info.each do |detail, names|
+      names.each do |name|
+        org_pigeon[name]
+        org_pigeon[name] [assign_to] ||= []
+        org_pigeon[name] [assign_to] << detail.to_s
+      end
+    end
+  end
+  org_pigeon
 end
